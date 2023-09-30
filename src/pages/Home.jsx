3a9ec81bdc77af62c5d452c03102a2218d8b188a
lil-aleks <olekiwan4@gmail.com>
2023-09-30 19:52:00 +0200

    Update files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ export const Home = () => {
     return (
         <React.StrictMode>
             <h1>lil-Client</h1>
+            <p>Ein Minecraft Client für jede art von Spieler.</p>
         </React.StrictMode>
     );
 
