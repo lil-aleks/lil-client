55b599e5e8ac28be3aa12c68c2a7abcb37cf1344
lil-aleks <olekiwan4@gmail.com>
2023-09-30 12:52:49 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './css/App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+ 
+export const App = () => {
+
+    return (
+        <React.StrictMode>
+            <div className='glow-card'>
+            <Router>
+                <Routes>
+                    
+                </Routes>
+            </Router>
+            </div>
+        </React.StrictMode>
+    );
+
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Home = () => {
+
+    return (
+        <React.StrictMode>
+            
+        </React.StrictMode>
+    );
+
+};
