6f3e0de3ea53d2e584f611a68b8a3fe19697012c
lil-aleks <olekiwan4@gmail.com>
2023-10-02 19:27:31 +0200

    Added 404 page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './css/App.css';
 import { Home } from './pages/Home';
+import { Error } from './pages/404Page';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
  
 export const App = () => {
@@ -12,6 +13,7 @@ export const App = () => {
                     <div className="glow-card">
                         <Routes>
                             <Route path='lil-client/' element={<Home/>}/>
+                            <Route path='lil-client/*' element={<Error/>}/>
                         </Routes>
                     </div>
                 </Router>
