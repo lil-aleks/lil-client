37249219e846ee7825ffd467a41f73edad7a4190
lil-aleks <olekiwan4@gmail.com>
2023-10-05 17:48:42 +0200

    Using https://github.com/rafgraph/spa-github-pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export const App = () => {
     return (
         <React.StrictMode>
 
-                <Router>
+                <Router basename='/lil-client'>
                     <div className="glow-card">
                         <select className='lang-selector' onChange={e => {
 
@@ -26,9 +26,9 @@ export const App = () => {
                             <option value="en">English</option>
                         </select>
                         <Routes>
-                            <Route path='lil-client/' element={<Home/>}/>
-                            <Route path='lil-client/*' element={<Error/>}/>
-                            <Route path='lil-client/test/' element={<Home/>}/>
+                            <Route path='/' element={<Home/>}/>
+                            <Route path='/*' element={<Error/>}/>
+                            <Route path='/test' element={<Home/>}/>
                         </Routes>
                     </div>
                 </Router>
